129e8f64893f933176b820cc4fcbeeff0d117a03
anirbaneukti <anirban.eukti@gmail.com>
2013-11-08 13:03:39 +0530

    added a view class

--- js/main.js
+++ js/main.js
@@ -21,26 +21,12 @@ var app = {
 		}
 	},*/
 	
-	renderHomeView: function() {
-		$('body').html(this.homeTpl());
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
-	
-	findByName: function() {
-		var self = this;
-		this.store.findByName($('.search-key').val(), function(employees) {
-			$('.employee-list').html(self.employeeLiTpl(employees));
-		});
-	},
-
     initialize: function() {
-		var self = this;
-		this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+		var self = this;		
         //this.store = new MemoryStore();
         this.store = new WebSqlStore(function() {
 			/*self.showAlert('Store Initialized', 'Info');*/
-			self.renderHomeView();
+			$('body').html(new HomeView(self.store).render().el);
 		});
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
