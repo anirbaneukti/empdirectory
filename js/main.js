b4bfd2e3166cb519cc22502343062bf90434ca15
anirbaneukti <anirban.eukti@gmail.com>
2013-11-08 11:44:41 +0530

    changed to use single html page and to handle html from js

--- js/main.js
+++ js/main.js
@@ -12,6 +12,25 @@ var app = {
             }
         });
     },
+    
+    /*showAlert: function (message, title) {
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
+	},*/
+	
+	renderHomeView: function() {
+		var html =
+				"<div class='header'><h1>Home</h1></div>" +
+				"<div class='search-view'>" +
+				"<input class='search-key'/>" +
+				"<ul class='employee-list'></ul>" +
+				"</div>"
+		$('body').html(html);
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	},
 	
     initialize: function() {
 		var self = this;
@@ -22,26 +41,6 @@ var app = {
 		});
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
-    
-    /*showAlert: function (message, title) {
-		if (navigator.notification) {
-			navigator.notification.alert(message, null, title, 'OK');
-		} else {
-			alert(title ? (title + ": " + message) : message);
-		}
-	},*/
-	
-    renderHomeView: function() {
-		var html =
-				"<div class='header'><h1>Home</h1></div>" +
-				"<div class='search-view'>" +
-				"<input class='search-key'/>" +
-				"<ul class='employee-list'></ul>" +
-				"</div>"
-		$('body').html(html);
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
-
 };
 
 app.initialize();
